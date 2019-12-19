6165cccc1796bce398826b54c39aad2477b709f2
Alex Wailan <alex.wailan24@gmail.com>
2019-12-19 14:36:10 +1000

    Update germie.py

--- germie.py
+++ germie.py
@@ -20,13 +20,7 @@ import shutil
 # Check if dependent programs are loaded #
 ##########################################
 
-dependencies = [
-'iqtree',
-'pyjar.py'
-]
 
-print()
-print("Checking dependencies mate! \n")
 def depend_check(dependencies):
     all_d = []
     for d in dependencies:
@@ -38,11 +32,6 @@ def depend_check(dependencies):
             all_d.append('FALSE')
     return all_d
 
-if not 'FALSE' in depend_check(dependencies):
-    print("I can see all dependencies! \n")
-else:
-    print("\n Mate! Not all required dependencies are loaded.")
-    sys.exit()
 
 
 
@@ -56,45 +45,20 @@ def getargv():
     parser = argparse.ArgumentParser(usage=usage, description=description)
 
     parser.add_argument('aln', action="store", help='Provide the alignment file.', metavar='N',nargs='?')
-    parser.add_argument('tree', action="store", help='Prefix of output JAR tree.', metavar='N', type=str, nargs='?')
+    parser.add_argument('tree', action="store", help='Name of output JAR tree.', metavar='N', type=str, nargs='?',default='tree')
     parser.add_argument('-d',    '--dirpath',  action="store",dest="pdir", help='Input directory containing alignment. End with a forward slash. Eg. /temp/fasta/', metavar='N', type=str, nargs='?', default=os.getcwd()+'/')
     parser.add_argument('-o',    '--outdir', action="store", dest="odir", help='Output directory. End with a forward slash. Eg. /temp/fasta/; Default to use current directory.', metavar='N', type=str, nargs='?', default=os.getcwd()+'/')
     return parser.parse_args()
 
+##########################################
+# How one communicates an error
+##########################################
 
-args = getargv()
-
-##the project directory that holds the alignment file
-idir = args.pdir
-
-##the project directory that holds the output
-odir = args.odir
-
-#reading in aln file
-aln = args.aln
-
-#reading in tree name
-tree = args.tree
-
-##where do you want the output to go
-
-##if the project directory and output directory don't have a forward slash exit
-if(idir[-1]!='/'):
-  print(idir[-1])
-  print('\n The project directory should end with a forward slash')
-  sys.exit()
-
-#Let your peeps know what is happening. Just a bit of communication.
-print(" ")
-print('Working directory will be: ' + idir)
-print('Output directory will be: ' + odir)
-print('Using alignment file: ' + aln)
-print('Output JAR tree will be named: ' + tree)
-
-print("Time to germinate!")
-
-aln_f = Path(idir+aln)
-
+def ErrorOut(error):
+    print ("\nError: ", error)
+    print ("\nThat's pretty sad face. Double check all inputs using -h or --help. Or call me ... maybe?")
+    print ()
+    sys.exit()
 
 ##########################################
 #                                        #
@@ -102,44 +66,102 @@ aln_f = Path(idir+aln)
 #                                        #
 ##########################################
 
-##########################################
-# Check if alignment file exists
-##########################################
+def main():
+ #############################################################################################
+    #
+    #      Parse/ check the arguements        
+    #
+    #############################################################################################
+    
+    args = getargv()
+    
+    if args.aln == None:
+        ErrorOut('No alignment file stated.')
+    elif args.tree == 'tree':
+        print('No tree name file was stated. The name "tree" will be used!')
 
-if not os.path.isfile(aln_f):
-    print("Unable to find the alignment file")
-    sys.exit()
-
-##########################################
-# Run IQ Tree to get that tree goodness
-##########################################
-
-
-p = subprocess.call("iqtree -s %s -m GTR+G -nt AUTO -bb 1000"%(aln_f), shell=True,    stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-
-tree_f = Path(idir+aln+'.treefile')
-
-##########################################
-# Check if that tree goodness exists
-##########################################
-
-
-if not os.path.isfile(tree_f):
-    print("IQtree has failed. Sad Face.")
-    sys.exit()
-
-##########################################
-# Run PyJAR for joint ancestral reconstruction
-##########################################
+    
+    idir = args.pdir ##the project directory that holds the alignment file
+    odir = args.odir ##the project directory that holds the output
+    aln = args.aln #reading in aln file
+    tree = args.tree #reading in tree name
 
 
-p = subprocess.call("pyjar.py -a %s -t %s -o %s"%(aln_f,tree_f,tree), shell=True,    stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-jar_f = Path(idir+tree+'.joint.tre')
+    ##if the project directory and output directory don't have a forward slash exit
+    if(idir[-1]!='/'):
+      print(idir[-1])
+      print('\n The project directory should end with a forward slash')
+      sys.exit()
+    
+        ##if the output directory and output directory doesn't have a forward slash exit
+    if(odir[-1]!='/'):
+      print(odir[-1])
+      print('\n The output directory should end with a forward slash')
+      exit()
+    
+    ##########################################
+    # Check if files exists
+    ##########################################
+    aln_f = Path(idir+aln)
+    if not os.path.isfile(aln_f):
+        ErrorOut("Unable to find the alignment file.")
+    
+    dependencies = [
+        'iqtree',
+        'pyjar.py'
+        ]
+    
+    print()
+    print("Checking dependencies mate! \n")
+    if not 'FALSE' in depend_check(dependencies):
+        print("I can see all dependencies! \n")
+    else:
+        print("\n Mate! Not all required dependencies are loaded.")
+        sys.exit()
 
-if not os.path.isfile(jar_f):
-    print("PyJar has failed. Sad Face.")
-    sys.exit()
 
-print()
-print("Germination complete! Enjoy your new tree mate!" )
-print("Its name is " + str(tree+'.joint.tre') + ". Please take care of it." )
+    #Let your peeps know what is happening. Just a bit of communication.
+    print(" ")
+    print('Working directory will be: ' + idir)
+    print('Output directory will be: ' + odir)
+    print('Using alignment file: ' + aln)
+    print('Output JAR tree will be named: ' + tree)
+    print("Time to germinate!")
+    
+
+    ##########################################
+    # Run IQ Tree to get that tree goodness
+    ##########################################
+
+
+    p = subprocess.call("iqtree -s %s -m GTR+G -nt AUTO -bb 1000"%(aln_f), shell=True,    stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+    tree_f = Path(idir+aln+'.treefile')
+
+    ##########################################
+    # Check if that tree goodness exists
+    ##########################################
+
+
+    if not os.path.isfile(tree_f):
+        print("IQtree has failed. Sad Face.")
+        sys.exit()
+
+    ##########################################
+    # Run PyJAR for joint ancestral reconstruction
+    ##########################################
+
+
+    p = subprocess.call("pyjar.py -a %s -t %s -o %s"%(aln_f,tree_f,tree), shell=True,    stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    jar_f = Path(idir+tree+'.joint.tre')
+
+    if not os.path.isfile(jar_f):
+        print("PyJar has failed. Sad Face.")
+        sys.exit()
+
+    print()
+    print("Germination complete! Enjoy your new tree mate!" )
+    print("Its name is " + str(tree+'.joint.tre') + ". Please take care of it." )
+    
+if __name__ == '__main__':
+    main()
